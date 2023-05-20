11050fe9555bb2646e2c928815028d7056d2d4c3
pedrolcorderor <pedrolcorderor@gmail.com>
2023-05-20 11:12:21 -0400

    productos

--- js/main.js
+++ js/main.js
@@ -2,185 +2,215 @@
 const productos = [
   // Abrigos
     {
-    id: "abrigo-01",
-    titulo: "Abrigo 01",
-    imagen: "./img/abrigos/01.jpg",
+    id: "mouse-01",
+    titulo: "mouse 01",
+    imagen: "./img/mouses/01.jpg",
     categoria: {
-        nombre: "Abrigos",
-        id: "abrigos",
+        nombre: "mouses",
+        id: "mouses",
     },
     precio: 1000,
     },
     {
-    id: "abrigo-02",
-    titulo: "Abrigo 02",
-    imagen: "./img/abrigos/02.jpg",
+    id: "mouse-02",
+    titulo: "mouse 02",
+    imagen: "./img/mouses/02.jpg",
     categoria: {
-        nombre: "Abrigos",
-        id: "abrigos",
+        nombre: "mouses",
+        id: "mouses",
     },
     precio: 1000,
   },
   {
-    id: "abrigo-03",
-    titulo: "Abrigo 03",
-    imagen: "./img/abrigos/03.jpg",
+    id: "mouse-03",
+    titulo: "mouse 03",
+    imagen: "./img/mouses/03.jpg",
     categoria: {
-      nombre: "Abrigos",
-      id: "abrigos",
+      nombre: "mouses",
+      id: "mouses",
     },
     precio: 1000,
   },
   {
-    id: "abrigo-04",
-    titulo: "Abrigo 04",
-    imagen: "./img/abrigos/04.jpg",
+    id: "mouse-04",
+    titulo: "mouse 04",
+    imagen: "./img/mouses/04.jpg",
     categoria: {
-      nombre: "Abrigos",
-      id: "abrigos",
+      nombre: "mouses",
+      id: "mouses",
     },
     precio: 1000,
   },
   {
-    id: "abrigo-05",
-    titulo: "Abrigo 05",
-    imagen: "./img/abrigos/05.jpg",
+    id: "mouse-05",
+    titulo: "mouse 05",
+    imagen: "./img/mouses/05.jpg",
     categoria: {
-      nombre: "Abrigos",
-      id: "abrigos",
+      nombre: "mouses",
+      id: "mouses",
     },
     precio: 1000,
   },
   // Camisetas
   {
-    id: "camiseta-01",
-    titulo: "Camiseta 01",
-    imagen: "./img/camisetas/01.jpg",
+    id: "casco-01",
+    titulo: "casco 01",
+    imagen: "./img/cascos/01.jpg",
     categoria: {
-      nombre: "Camisetas",
-      id: "camisetas",
+      nombre: "Cascos",
+      id: "cascos",
     },
     precio: 1000,
   },
   {
-    id: "camiseta-02",
-    titulo: "Camiseta 02",
-    imagen: "./img/camisetas/02.jpg",
+    id: "casco-02",
+    titulo: "casco 02",
+    imagen: "./img/cascos/02.jpg",
     categoria: {
-      nombre: "Camisetas",
-      id: "camisetas",
+      nombre: "Cascos",
+      id: "cascos",
     },
     precio: 1000,
   },
   {
-    id: "camiseta-03",
-    titulo: "Camiseta 03",
-    imagen: "./img/camisetas/03.jpg",
+    id: "casco-03",
+    titulo: "casco 03",
+    imagen: "./img/cascos/03.jpg",
     categoria: {
-      nombre: "Camisetas",
-      id: "camisetas",
+      nombre: "Cascos",
+      id: "cascos",
     },
     precio: 1000,
   },
   {
-    id: "camiseta-04",
-    titulo: "Camiseta 04",
-    imagen: "./img/camisetas/04.jpg",
+    id: "casco-04",
+    titulo: "casco 04",
+    imagen: "./img/cascos/04.jpg",
     categoria: {
-      nombre: "Camisetas",
-      id: "camisetas",
+      nombre: "Cascos",
+      id: "cascos",
     },
     precio: 1000,
   },
   {
-    id: "camiseta-05",
-    titulo: "Camiseta 05",
-    imagen: "./img/camisetas/05.jpg",
+    id: "casco-05",
+    titulo: "casco 05",
+    imagen: "./img/cascos/05.jpg",
     categoria: {
-      nombre: "Camisetas",
-      id: "camisetas",
+      nombre: "Cascos",
+      id: "cascos",
     },
     precio: 1000,
   },
   {
-    id: "camiseta-06",
-    titulo: "Camiseta 06",
-    imagen: "./img/camisetas/06.jpg",
+    id: "casco-06",
+    titulo: "casco 06",
+    imagen: "./img/cascos/06.jpg",
     categoria: {
-      nombre: "Camisetas",
-      id: "camisetas",
+      nombre: "Cascos",
+      id: "cascos",
     },
     precio: 1000,
   },
   {
-    id: "camiseta-07",
-    titulo: "Camiseta 07",
-    imagen: "./img/camisetas/07.jpg",
+    id: "casco-07",
+    titulo: "casco 07",
+    imagen: "./img/cascos/07.jpg",
     categoria: {
-      nombre: "Camisetas",
-      id: "camisetas",
+      nombre: "Cascos",
+      id: "cascos",
     },
     precio: 1000,
   },
   {
-    id: "camiseta-08",
-    titulo: "Camiseta 08",
-    imagen: "./img/camisetas/08.jpg",
+    id: "casco-08",
+    titulo: "casco 08",
+    imagen: "./img/cascos/08.jpg",
     categoria: {
-      nombre: "Camisetas",
-      id: "camisetas",
+      nombre: "Cascos",
+      id: "cascos",
     },
     precio: 1000,
   },
   // Pantalones
   {
-    id: "pantalon-01",
-    titulo: "Pantalón 01",
-    imagen: "./img/pantalones/01.jpg",
+    id: "lapto-01",
+    titulo: "lapto 01",
+    imagen: "./img/laptos/01.jpg",
     categoria: {
-      nombre: "Pantalones",
-      id: "pantalones",
+      nombre: "Laptos",
+      id: "laptos",
     },
     precio: 1000,
   },
   {
-    id: "pantalon-02",
-    titulo: "Pantalón 02",
-    imagen: "./img/pantalones/02.jpg",
+    id: "lapto-02",
+    titulo: "lapto 02",
+    imagen: "./img/laptos/02.jpg",
     categoria: {
-      nombre: "Pantalones",
-      id: "pantalones",
+      nombre: "Laptos",
+      id: "laptos",
     },
     precio: 1000,
   },
   {
-    id: "pantalon-03",
-    titulo: "Pantalón 03",
-    imagen: "./img/pantalones/03.jpg",
+    id: "lapto-03",
+    titulo: "lapto 03",
+    imagen: "./img/laptos/03.jpg",
     categoria: {
-      nombre: "Pantalones",
-      id: "pantalones",
+      nombre: "Laptos",
+      id: "laptos",
     },
     precio: 1000,
   },
   {
-    id: "pantalon-04",
-    titulo: "Pantalón 04",
-    imagen: "./img/pantalones/04.jpg",
+    id: "lapto-04",
+    titulo: "lapto 04",
+    imagen: "./img/laptos/04.jpg",
     categoria: {
-      nombre: "Pantalones",
-      id: "pantalones",
+      nombre: "Laptos",
+      id: "laptos",
     },
     precio: 1000,
   },
   {
-    id: "pantalon-05",
-    titulo: "Pantalón 05",
-    imagen: "./img/pantalones/05.jpg",
+    id: "lapto-05",
+    titulo: "lapto 05",
+    imagen: "./img/laptos/05.jpg",
     categoria: {
-      nombre: "Pantalones",
-      id: "pantalones",
+      nombre: "Laptos",
+      id: "laptos",
     },
     precio: 1000,
   },
 ];
+
+const contenedorProductos = document.querySelector("#contenedor-Productos");
+function cargarProductos() {
+  productos.forEach(producto => {
+    const div = document.createElement("div");
+    div.classList.add("producto");
+    div.innerHTML = `
+                    <img class="producto-imagen" src="${producto.imagen}" alt="">
+                    <div class="producto-detalles">
+                        <h3 class="producto-titulo">${producto.titulo}</h3>
+                        <p class="producto-precio">${producto.precio}</p>
+                        <button class="producto-agregar" id="${producto.id}">Agregar</button>
+                    </div>
+    `;
+    contenedorProductos.append(div);
+  });
+  /*  por cada uno de los elemento creara un div , le agregamos la clase del css y implementamos el contenido que tendra el producto. luego a cada divv
+ sera agregado al contendor y luego ejecutamos la fucion */
+};
+cargarProductos();
+
+/* 
+                <div class="producto">
+                    <img class="producto-imagen" src="./img/cascos/01.jpg" alt="">
+                    <div class="producto-detalles">
+                        <h3 class="producto-titulo">casco 01</h3>
+                        <p class="producto-precio">$200</p>
+                        <button class="producto-agregar">Agregar</button>
+                    </div>
+   </div> */
